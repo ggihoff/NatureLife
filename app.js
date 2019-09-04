d6cb9d78d0e79d25a4d304df398e1b1aeeaf2513
ggihoff <52943581+ggihoff@users.noreply.github.com>
2019-09-04 03:16:19 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+const navSlide = () => {
+    const burguer = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+
+    burguer.addEventListener('click',() => {
+        
+        nav.classList.toggle('nav-active');
+    });
+}
+
+
+navSlide();
+
+
+
+
+
