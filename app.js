e2700e07e81c9a664be4e4c0d6790894ca4eaa81
ggihoff <52943581+ggihoff@users.noreply.github.com>
2019-09-08 15:24:29 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,25 +1,49 @@
 
+var activo = false;
 $(window).on('scroll', function(){
 if ($(window).scrollTop()){
+
+       
+
+
         $('nav').addClass('black');
 
     }
     else {
-        $('nav').removeClass('black');
+     
+        $('nav').removeClass('black');  
+        
+
+
     }
 })
-
 
 
 const navSlide = () => {
     const burguer = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
+ 
 
     burguer.addEventListener('click',() => {
         
+
         nav.classList.toggle('nav-active');
+        $('nav').addClass('black');
+
+        if (activo == false) {
+        activo = true;
+        }
+       else {
+        $('nav').removeClass('black');
+        activo = false;
+       }
+
+       console.log(activo);
     });
 }
+
+
+
 
 
 navSlide();
